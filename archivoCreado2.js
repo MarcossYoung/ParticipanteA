74d54b0f3b247a86ee377840d4a13339d594f281
Hernan Cerda <cerda@ARBUEN10R1128.ga.local>
2021-07-14 21:46:46 -0300

    ultima  mod en archivoCreado2

--- archivoCreado2.js
+++ archivoCreado2.js
@@ -2,3 +2,9 @@ console.log("archivo creado2")
 /// para poder pusherlo lo tuve que comitear primero
 /// segunda mod en archivoCreado2
 /// y tambien hacerle una add
+/// en conclusion los paso que realice son:
+/// 1- modifiqué el archivo. js local y guardé el cambio
+/// 2- ejecuté un git add ""
+/// 3- ejecuté un commit -m
+/// 4- ejecuté un push -u
+/// y se ve el cambio en github
